35f98642c7d0de9f60ba824d6817219acf29202d
Mohmmed <Mohmmed@DESKTOP-T69I045>
2019-11-27 22:01:25 -0900

    xxxx

--- WebApplication1/WebApplication1/Controllers/OmdaTestController.cs
+++ WebApplication1/WebApplication1/Controllers/OmdaTestController.cs
@@ -9,7 +9,7 @@ namespace WebApplication1.Controllers
     public class OmdaTestController : Controller
     {
         // GET: OmdaTest
-        public ActionResult Index()
+        public ActionResult Index222()
         {
             return View();
         }
